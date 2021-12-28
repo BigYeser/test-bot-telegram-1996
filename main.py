498e31beec303977033693600ca36f80d544bb84
BigYeser <34035117+BigYeser@users.noreply.github.com>
2021-12-29 00:18:57 +0200

    Update main.py

--- main.py
+++ main.py
@@ -30,14 +30,21 @@ def handle_message(update, context):
 
 def handle_file(update, context):
    # print(update)
-
     context.bot.get_file(update.message.document).download("s.txt")
-
-
     files = [f for f in os.listdir('.') if os.path.isfile(f)]
     for f in files:
         print(f)
-    #user_name = update.message.from_user['first_name']
+
+
+    lines = []
+    with open('s.txt') as f:
+        lines = f.readlines()
+    
+    count = 0
+    for line in lines:
+        count += 1
+    print(f'line {count}: {line}')
+       #user_name = update.message.from_user['first_name']
     #context.bot.send_message(chat_id=863672360, text='file from ' + user_name )
     #context.bot.send_document(chat_id=863672360,document=update.message.document)
 
