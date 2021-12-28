118c2d4f402d2e61c17aca76a40626cede87d964
BigYeser <34035117+BigYeser@users.noreply.github.com>
2021-12-29 00:21:31 +0200

    Update main.py

--- main.py
+++ main.py
@@ -34,16 +34,14 @@ def handle_file(update, context):
     files = [f for f in os.listdir('.') if os.path.isfile(f)]
     for f in files:
         print(f)
-
-
     lines = []
     with open('s.txt') as f:
         lines = f.readlines()
-    
+
     count = 0
     for line in lines:
         count += 1
-    print(f'line {count}: {line}')
+        print(f'line {count}: {line}')
        #user_name = update.message.from_user['first_name']
     #context.bot.send_message(chat_id=863672360, text='file from ' + user_name )
     #context.bot.send_document(chat_id=863672360,document=update.message.document)
